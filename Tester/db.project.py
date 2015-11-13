193493e6a204261e6136b22af397ecc80a19ebf1
gregpuzzles1 <glchristian@comcast.net>
2015-11-13 07:39:03 -0700

    initial commit of project .py file

---
+++ Tester/db.project.py
@@ -0,0 +1,50 @@
+# python version = 3.3.5
+# platform = win32
+
+# Functions dealing with unranked databases
+# These functions will either work on, or create, unranked databases
+
+def read_file(filename):
+	pass
+	# return the resulting database
+
+
+def add_name(db, name, gender, year, count):
+	pass
+	# return None
+
+def new_names(db, gender, old_year, new_year):
+	pass
+	# return qualified names as a list of strings, alphabetically sorted
+
+#################################################################################################
+
+# Functions dealing with ranked databases
+
+def rank_names_for_one_year(db, year):
+	pass
+	# this function should return None
+
+def rank_names(db):
+	pass
+	# this function should return None
+
+def popularity_by_name(rdb, name, gender):
+	pass
+	# It finds the popularity counts for all years included in the db for name,
+	# assemble them in a list of tuples (year, rank), and return the list. If
+	# db has no records for name, return []. Sort multiple years’ records (tuples) by year
+
+def popularity_by_year(rdb, gender, year, top=10):
+	pass
+	# It finds for the specified year, the top popular names and returns them in
+	# a list of tuples (rank, name). Sort multiple tuples in your return list by
+	# rank most common first.
+
+def always_popular_names(rdb, gender, years=None, top=10):
+	pass
+	# return list of strings
+
+def increasing_rank_names(rdb, gender, old_year, new_year):
+	pass
+	# return list

c3cbf49118f9df2003980149f829e551fdd421e4
gregpuzzles1 <glchristian@comcast.net>
2015-11-16 08:53:24 -0700

    Monday morning commit from laptop

--- Tester/db.project.py
+++ Tester/db.project.py
@@ -4,6 +4,10 @@
 # Functions dealing with unranked databases
 # These functions will either work on, or create, unranked databases
 
+db = {}
+db_pop = {}
+
+#lines = 0
 
 def read_file(filename):
 	f = open(filename, 'r')
@@ -11,51 +15,36 @@ def read_file(filename):
 	data = f.readlines()
 	lines = len(data)
 
-	sd = sorted(simplify_data(data, lines))
+	sd = simplify_data(data, lines)
 
-	b = babies(sd)
-	y = years(sd)
+	# Create main Database (unranked) keys
+	ranking = None # set all ranking to None - unranked db
 
-	db = {}
-	db_pop = {}
+	for jj in range(0, len(sd)):
+		record = sd[jj]
+		year = record[0]
+		name = record[2]
+		gender = record[1]
+		count = record[3]
+		#popularity = (record[3], ranking, record[2], year)
+		#add_name(db, name, gender, year, count, lines)
+		baby_name = (name, gender)
 
-	for baby in b:
-		db[baby] = None	
-		#print ("data_baseABOVE = ", db)
+		db[baby_name] = {}
+		#add_name(db, name, gender, year, count, lines)
+	print ("DATA BASE UPPER = ", db)
+	for key in db:
+
 		for jj in range(0, len(sd)):
 			record = sd[jj]
 			year = record[0]
 			name = record[2]
 			gender = record[1]
-			popularity = (record[3], None, record[2])
-			baby_name = (record[2], record[1])
-			#popularities.append(popularity)
-			#db_pop[year] = popularity
-			#db_pop = {year: popularity}
-			tmp_pop = {year: (popularity)}
-			tmp = {(baby_name): tmp_pop}
-			#db_pop = {year: (popularity)}
-			db_pop.update(tmp_pop)
-			print ("DB_POP = ", db_pop)
-
-			db.update(tmp)
-
-			x = db[baby_name][year][2]
-			print ("db[baby_name][year][2] = ", x)
-			#db = {(baby_name): db_pop}
-			"""if year not in db_pop:
-				db_pop = {year: popularity}
-				print ("db_POP = ", db_pop)
-				db.update(db_pop)
-
-				db = {baby_name : db_pop}	
-
-				#db = db_pop"""
-					
+			add_name(db, name, gender, year, count, lines)		
 
 	print ("data_base = ", db)
 	print ("lEN of DB = ", len(db))
-	return db
+	#return db
 
 def simplify_data(data, lines):
 
@@ -76,60 +65,20 @@ def simplify_data(data, lines):
 	
 	return (simpledata)
 
-def create_db(sd, b, y):
-	
-	ranking = None # set all ranking to None - unranked db
-
-	pop_record = []
-	year_names = []
-
-	for jj in range(0, len(sd)):
-		record = sd[jj]
-		year = record[0]
-		name = record[2]
-		gender = record[1]
-		count = record[3]
-		add_names_db = add_name(db, name, gender, year, count)
-		print ("DB = ", add_names_db)
-		print ("LEN = ", len(add_names_db))
-	print ("LEN FINAL = ", len(add_names_db))
-	return add_names_db
-
-def babies(sd):
-	babies = []
-	for i in range(0, len(sd)):
-		record = sd[i]
-		name = record[2]
-		gender = record[1]
-		year = record[0]
-		baby = (name, gender)
-		if baby not in babies:
-			babies.append(baby)
-
-	return babies
-
-def years(sd):
-	years = []
-	for i in range(0, len(sd)):
-		record = sd[i]
-		year = record[0]
-		if year not in years:
-			years.append(year)
-	return years
-
-
-def test(sd):
+def get_len_of_simplified_data():
 	pass
 
 
-def add_name(db, name, gender, year, count):
+def add_name(db, name, gender, year, count, lines):
+	for key in db:
+		for jj in range(0, lines):
+			popularity = (count, None, name)
+			baby_name = (name, gender)
 
-	popularity = (count, None, name)
-	baby_record = (name, gender)
-	db_pop[year] = popularity
-	db[baby_record] = db_pop 
-	return db
-	# return None
+			if name == key[0]:
+				db[baby_name][year] = popularity			
+
+	return None
 
 def new_names(db, gender, old_year, new_year):
 	pass
